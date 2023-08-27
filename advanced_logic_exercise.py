7a59688e270ec7845716ef203670b09e5758c613
Darren Lackie <lackiewwpj@gmail.com>
2023-08-27 20:59:43 +0100

    uploading first 3 answers of advanced_logic_exercises

---
+++ advanced_logic_exercise.py
@@ -0,0 +1,62 @@
+# For the following list of numbers:
+
+numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+
+# 1. Print out a list of the even integers:
+even_integers = []
+
+for number in numbers:
+    if number % 2 == 0:
+        even_integers.append(number)
+
+print(even_integers)
+
+# 2. Print the difference between the largest and smallest value:
+
+largest_value = numbers[0]
+smallest_value = numbers[0]
+
+for number in numbers:
+    if number > largest_value:
+        largest_value = number
+    if number < smallest_value:
+        smallest_value = number
+difference_between_values = largest_value - smallest_value
+
+print(difference_between_values)
+
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+
+# print(range(len(numbers) - 1)) #prints range 0, 9. Allows you to check last index
+
+for index in range(len(numbers) - 1):
+        if numbers[index] == (numbers[index + 1]):
+            same_numbers = True
+            break
+        else:
+            same_numbers = False
+
+print(same_numbers)
+
+
+# 4. Print the sum of the numbers, 
+#    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
+#    
+#    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+
+
+
+# 5. HARD! Print the sum of the numbers. 
+#    Except the number 13 is very unlucky, so it does not count.
+#    And numbers that come immediately after a 13 also do not count.
+#    HINT - You will need to track the index throughout the loop.
+#
+#    So [5, 13, 2] would have sum of 5. 
+
+
+
+
+
+
+
